736b6f648ec8b2bf706ea1e9ee9c71b8f34695a7
Alekseyka <harlan14@mail.ru>
2025-07-20 15:23:24 +0700

    CsvFileSource

--- src/test/java/ru/Netology/javaMvn/FreelanceServesTest.java
+++ src/test/java/ru/Netology/javaMvn/FreelanceServesTest.java
@@ -3,24 +3,18 @@ package ru.Netology.javaMvn;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class FreelanceServesTest {
 
     @ParameterizedTest
-    @CsvSource({
-            "10000, 3000, 20000, 3",
-            "100000, 60000, 150000, 2",
-            "5000, 6000, 10000, 0"
-    })
 
+    @CsvFileSource(files = "src/test/resources/dataTest.csv")
     public void testFirst(int income, int expenses, int threshold, int expected) {
         FreelanceServes service = new FreelanceServes();
 
         int actual = service.calculateMonthsOfRest(income, expenses, threshold);
-
-        //System.out.println(vocation);
-
 
         Assertions.assertEquals(expected, actual);
     }
