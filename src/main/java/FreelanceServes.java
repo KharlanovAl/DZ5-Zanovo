55bb1b175870ef6d7b41a12f291163c7c51642d5
Alekseyka <harlan14@mail.ru>
2025-07-20 14:34:51 +0700

    HomeWork in Zanovo

---
+++ src/main/java/FreelanceServes.java
@@ -0,0 +1,23 @@
+public class FreelanceServes {
+
+
+    public int calculateMonthsOfRest(int income, int expenses, int threshold) {
+        int money = 0;
+        int monthsOfRest = 0;
+
+        for (int month = 1; month <= 12; month++) {
+            if (money >= threshold) {
+                    // Отдых
+                    money -= expenses;
+                    money /= 3;
+                    monthsOfRest++;
+            } else {
+                    // Работа
+                    money += income;
+                    money -= expenses;
+            }
+        }
+
+            return monthsOfRest;
+        }
+    }
---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        FreelanceServes service = new FreelanceServes();
+
+        int vocation = service.calculateMonthsOfRest( 10000, 3000, 20000);
+
+        System.out.println(vocation);
+    }
+
+}
